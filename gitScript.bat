1a4f69cd5576463c60834cb7bbd83c3734be8f55
githubtvp <tvp1640@gmail.com>
2024-02-04 22:56:55 +0530

    Saving latest Updates with script

--- gitScript.bat
+++ gitScript.bat
@@ -1,5 +1,5 @@
 ::::git pull origin main
-git remote add origin https://github.com/githubtvp/23MXAG.git
+::::git remote add origin https://github.com/githubtvp/23MXAG.git
 git pull origin main --allow-unrelated-histories
 git add .
 git commit -a -m "Saving latest Updates with script"
